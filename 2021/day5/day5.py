de0fab9b32299f5816248fe2480b9fb7fc9860ca
z3y50n <dim_papag@windowslive.com>
2021-12-05 22:24:29 +0200

    use black!

--- 2021/day5/day5.py
+++ 2021/day5/day5.py
@@ -8,42 +8,47 @@ def read_data(filename):
 t_lines = read_data("test_input.txt")
 lines = read_data("input.txt")
 
+
 def is_horizontal(line):
     if line[0][0] == line[1][0]:
         return True
     return False
+
 
 def is_vertical(line):
     if line[0][1] == line[1][1]:
         return True
     return False
 
+
 def print_board(board):
     for row in board:
         print(row)
 
+
 # PART 1
 def part1(lines):
-    board = [[0 for j in range(1000)] for i in range(1000)] #1000 is cheating:P
+    board = [[0 for j in range(1000)] for i in range(1000)]  # 1000 is cheating:P
     for line in lines:
         if is_horizontal(line):
             f_y = min(line[0][1], line[1][1])
             t_y = max(line[0][1], line[1][1])
-            for i in range(f_y, t_y+1):
+            for i in range(f_y, t_y + 1):
                 board[i][line[0][0]] += 1
         elif is_vertical(line):
             f_x = min(line[0][0], line[1][0])
             t_x = max(line[0][0], line[1][0])
-            for i in range(f_x, t_x+1):
+            for i in range(f_x, t_x + 1):
                 board[line[0][1]][i] += 1
     return sum(i > 1 for row in board for i in row)
+
 
 assert part1(t_lines) == 5
 print(part1(lines))
 
 # PART 2
 def part2(lines):
-    board = [[0 for j in range(1000)] for i in range(1000)] #1000 is cheating:P
+    board = [[0 for j in range(1000)] for i in range(1000)]  # 1000 is cheating:P
     for line in lines:
         f_x = line[0][0]
         f_y = line[0][1]
@@ -63,6 +68,7 @@ def part2(lines):
         board[f_y][f_x] += 1
 
     return sum(i > 1 for row in board for i in row)
+
 
 assert part2(t_lines) == 12
 print(part2(lines))
