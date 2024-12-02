d41f109a4f0de668e1d432dd2c7f899c26a5d1e7
z3y50n <dim_papag@windowslive.com>
2024-12-02 20:47:54 +0200

    Day2 part 2

--- 2024/day02/main.py
+++ 2024/day02/main.py
@@ -4,18 +4,22 @@ def read_data(filename):
     return [list(map(int, row.split())) for row in data]
 
 
-def part1(reports: list[list[int]]) -> int:
-    safes = 0
-    for report in reports:
-        diffs = []
-        for i in range(1, len(report)):
-            diffs.append(report[i] - report[i - 1])
-        if (
-            all([diff > 0 for diff in diffs]) or all([diff < 0 for diff in diffs])
-        ) and all([0 < abs(diff) < 4 for diff in diffs]):
-            safes += 1
+def is_safe(report: list[int]) -> bool:
+    diffs = [report[i] - report[i - 1] for i in range(1, len(report))]
+    return (
+        all([diff > 0 for diff in diffs]) or all([diff < 0 for diff in diffs])
+    ) and all([0 < abs(diff) < 4 for diff in diffs])
 
-    return safes
+
+def part1(reports: list[list[int]]) -> int:
+    return sum(is_safe(report) for report in reports)
+
+
+def part2(reports: list[list[int]]) -> int:
+    return sum(
+        any(is_safe(report[:j] + report[j + 1 :]) for j in range(len(report)))
+        for report in reports
+    )
 
 
 if __name__ == "__main__":
@@ -23,3 +27,5 @@ if __name__ == "__main__":
     test_reports = read_data("test_input")
     assert (part1(test_reports)) == 2
     print(part1(reports))
+    assert part2(test_reports) == 4
+    print(part2(reports))
